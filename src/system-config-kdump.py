dde27e84f0dc73afdc56258c2cafcfd66fa81865
Roman Rakus <rrakus@redhat.com>
2010-08-16 13:45:20 +0200

    dbus proxy in separate module
    DBusProxy class moved to sckdump/dbus_proxy module
    
    Signed-off-by: Roman Rakus <rrakus@redhat.com>

--- src/system-config-kdump.py
+++ src/system-config-kdump.py
@@ -33,12 +33,14 @@ import sckdump.dialogs as dialogs
 # progress window
 from sckdump.progress import ProgressWindow
 
+# dbus proxy
+from sckdump.dbus_proxy import DBusProxy
+
 ##
 ## dbus and polkit
 ##
 import dbus
 import slip.dbus.service
-from slip.dbus import polkit
 
 ##
 ## I18N
@@ -147,77 +149,6 @@ tab 3 initrd selection
 localizations
 XEN support - need change in grubby
 """
-
-# all needed for Python-slip, PoilcyKit and dbus
-class DBusProxy (object):
-    """
-    Class used for communication with/via dbus
-    """
-
-    def __init__ (self):
-        self.bus = dbus.SystemBus ()
-        self.dbus_object = self.bus.get_object (
-            "org.fedoraproject.systemconfig.kdump.mechanism",
-            "/org/fedoraproject/systemconfig/kdump/object")
-
-    @polkit.enable_proxy
-    def getdefaultkernel (self):
-        """
-        Return name of default kernel set in bootloader configuration string
-        """
-        return self.dbus_object.getdefaultkernel (dbus_interface = "org.fedoraproject.systemconfig.kdump.mechanism")
-
-    @polkit.enable_proxy
-    def getcmdline (self, kernel):
-        """
-        Return command line arguments for specific kernel
-        """
-        return self.dbus_object.getcmdline (kernel, dbus_interface = "org.fedoraproject.systemconfig.kdump.mechanism")
-
-    @polkit.enable_proxy
-    def getxencmdline (self, kernel):
-        """
-        Return command line arguments for specific xen kernel
-        """
-        return self.dbus_object.getxencmdline (kernel, dbus_interface = "org.fedoraproject.systemconfig.kdump.mechanism")
-
-    @polkit.enable_proxy
-    def getallkernels (self):
-        """
-        Return name of all kernels in bootloader configuration files separated by newline (\n)
-        """
-        return self.dbus_object.getallkernels (dbus_interface = "org.fedoraproject.systemconfig.kdump.mechanism")
-
-    @polkit.enable_proxy
-    def writedumpconfig (self, config_string):
-        """
-        Write config_string string to kdump.conf file and returns same string if no errors
-        """
-        return self.dbus_object.writedumpconfig (config_string, dbus_interface = "org.fedoraproject.systemconfig.kdump.mechanism")
-
-    @polkit.enable_proxy
-    def writebootconfig (self, config_string):
-        """
-        Update bootloader configuration. config_string is grubby arguments. Return error messages, if any.
-        """
-        return self.dbus_object.writebootconfig (config_string, dbus_interface = "org.fedoraproject.systemconfig.kdump.mechanism")
-
-    @polkit.enable_proxy
-    def handlekdumpservice(self, chkconfig_status, service_op):
-        """
-        Start or stop kdump service, turn on or off init script
-        """
-        return self.dbus_object.handledumpservice (
-            (chkconfig_status, service_op),
-            dbus_interface = "org.fedoraproject.systemconfig.kdump.mechanism")
-
-    @polkit.enable_proxy
-    def getservicestatus(self):
-        """
-        Get the current status of the kdump service
-        """
-        return self.dbus_object.getservicestatus (
-            dbus_interface = "org.fedoraproject.systemconfig.kdump.mechanism")
 
 # This class contains every settings
 class Settings:
